cb503d7f4a13f167c71bb188e9aee34cd42d3e63
Dean Fox <deanfoxcd@gmail.com>
2024-12-20 20:42:16 -0500

    add searchOrder functionality

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 export default function Header() {
   return (
     <header>
       <Link to="/">Fat React Pizza Co.</Link>
+      <SearchOrder />
       <p>Dean</p>
     </header>
   );
