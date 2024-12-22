0556c91a1577e83a6376e3e28fb601e9f173f902
Dean Fox <deanfoxcd@gmail.com>
2024-12-21 21:33:17 -0500

    add more styling (buttons and inputs)

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchOrder() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
   function handleSubmit(e) {
@@ -10,7 +10,7 @@ export default function SearchOrder() {
     if (!query) return;
 
     navigate(`/order/${query}`);
-    setQuery('');
+    setQuery("");
   }
 
   return (
@@ -19,6 +19,7 @@ export default function SearchOrder() {
         placeholder="Search for an order"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
