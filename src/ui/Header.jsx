47cf53bb1226813fd11de7562f50a1fc54438b87
Dean Fox <deanfoxcd@gmail.com>
2024-12-23 09:58:40 -0500

    style buttons and menu

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fat React Pizza Co.
+        Fast React Pizza Co.
       </Link>
       <SearchOrder />
       <Username />
